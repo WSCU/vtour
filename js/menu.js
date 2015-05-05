83652141b3dfd3164348fd28a77ad286d05a8304
Luke <luktut@gmail.com>
2015-05-05 16:06:18 -0600

    Menu work

--- js/menu.js
+++ js/menu.js
@@ -6,17 +6,17 @@ function dispMenu(items) {
         mi = items[i];
         icon = "<i class='"+mi.icon+"'></i>";
 
-        if (mi.target == "locations") {
-            a = "<a href='"+mi.href+"'>" + icon + "&nbsp;"+mi.text+"</a>"
-        } else if (mi.target == "external") {
-            a = "<a href='"+mi.href+"' target='_blank'>" + icon + "&nbsp;"+mi.text+"</a>"
-        } else if (mi.target == "ci") {
+        if (mi.target === "locations") {
+            a = "<a href='"+mi.href+"'>" + icon + "&nbsp;"+mi.text+"</a>";
+        } else if (mi.target === "external") {
+            a = "<a href='"+mi.href+"' target='_blank'>" + icon + "&nbsp;"+mi.text+"</a>";
+        } else if (mi.target === "ci") {
             console.log("Not supported - Menu to carousel item");
         } else {
-            a = "<a href=''>"+ icon + "&nbsp;"+mi.text+"</a>"
+            a = "<a href=''>"+ icon + "&nbsp;"+mi.text+"</a>";
         }
 
-        inner += a
+        inner += a;
 
         if (mi.items.length > 0) {
             inner += "<ul>" + dispMenu(mi.items) + "</ul>";
@@ -27,7 +27,6 @@ function dispMenu(items) {
 }
 
 function dispMainMenu() {
-    console.log("hai");
     var inner = "";
     inner += "<li><a href='"+menu.target+"'><i class='"+menu.icon+"'></i>&nbsp; "+menu.text+"</a>"
     inner += "<ul>" + dispMenu(menu.items) + "</ul>";
